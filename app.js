68b2b26865cc8faa364ba5280ce95e1eafb19ee5
Caio Henrique <caiohenriquedev@gmail.com>
2023-08-11 21:14:52 -0300

    ok

---
+++ app.js
@@ -0,0 +1,100 @@
+const sucata_pesada = document.querySelector("#sucata_pesada")
+const sucata_mista = document.querySelector("#sucata_mista")
+const Aluminio = document.querySelector("#Aluminio")
+const Cobre = document.querySelector("#Cobre")
+const Metal = document.querySelector("#Metal")
+const Ferro_Fundido = document.querySelector("#Ferro_Fundido")
+const Ferro_Ruim = document.querySelector("#Ferro_Ruim")
+const caixas = document.querySelector("#caixa")
+const btn = document.querySelector("#liveToastBtn")
+const resultado = document.querySelector("#resultado")
+const Valor = document.querySelector(".toast-body");
+const dinheiro = document.querySelector("#red");
+
+
+
+
+
+
+
+
+btn.addEventListener("click", () => {
+
+    const valor_1 = sucata_pesada.value * localStorage.getItem("sucata_pesada");
+
+
+    const valor_2 = sucata_mista.value * localStorage.getItem("sucata_mista")
+
+
+    const valor_3 = Aluminio.value * localStorage.getItem("Aluminio")
+
+
+    const valor_4 = Cobre.value * localStorage.getItem("Cobre")
+
+
+    const valor_5 = Ferro_Fundido.value * localStorage.getItem("Ferro_Fundido")
+
+
+   
+
+
+       
+
+
+     if(sucata_pesada.value === ''){
+      sucata_pesada.value = 0 
+     }
+     if(sucata_mista.value === ''){
+       sucata_mista.value = 0 
+     }
+     if(Aluminio.value  === ''){
+       Aluminio.value  = 0 
+     }
+     if(Cobre.value === ''){
+       Cobre.value = 0 
+     }
+     if(Metal.value === ''){
+       Metal.value = 0 
+     }
+     if(Ferro_Fundido.value === ''){
+       Ferro_Fundido.value = 0 
+     }
+     if(Ferro_Ruim.value === ''){
+       Ferro_Ruim.value = 0 
+     }
+    if(caixas.value === "" ) {
+        caixas.value = 0;
+    }
+    
+
+
+    const valor_6 = Ferro_Ruim.value * localStorage.getItem("Ferro_Ruim")
+
+    const valor_7 = caixas.value * localStorage.getItem("caixa")
+
+    const valor_8 = Metal.value * localStorage.getItem("Metal")
+
+    
+
+ 
+
+    let pagar = valor_1 + valor_2 + valor_3 + valor_4 + valor_5 + valor_6 + valor_8;
+    
+
+    localStorage.setItem("pagar", pagar);
+
+    const total = localStorage.getItem("pagar")
+
+    Valor.innerHTML = `O valor a ser pago é R$ ${total}`
+
+})
+
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+
+if (toastTrigger) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+  toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show()
+  })
+}
---
+++ script.js
@@ -0,0 +1,32 @@
+const sucata_pesada = document.querySelector("#sucata_pesada")
+const sucata_mista = document.querySelector("#sucata_mista")
+const Aluminio = document.querySelector("#Aluminio")
+const Cobre = document.querySelector("#Cobre")
+const Metal = document.querySelector("#Metal")
+const Ferro_Fundido = document.querySelector("#Ferro_Fundido")
+const Ferro_Ruim = document.querySelector("#Ferro_Ruim")
+const caixas = document.querySelector("#caixa")
+const btn = document.querySelector("#btn")
+const resultado = document.querySelector("#resultado")
+
+
+
+btn.addEventListener("click" , () => {
+    localStorage.setItem("sucata_pesada", parseFloat(sucata_pesada.value));
+    localStorage.setItem("sucata_mista", parseFloat(sucata_mista.value));
+    localStorage.setItem("Aluminio", parseFloat(Aluminio.value));
+    localStorage.setItem("Cobre", parseFloat(Cobre.value));
+    localStorage.setItem("Metal", parseFloat(Metal.value));
+    localStorage.setItem("Ferro_Fundido", parseFloat(Ferro_Fundido.value));
+    localStorage.setItem("Ferro_Ruim", parseFloat(Ferro_Ruim.value));
+    localStorage.setItem("caixa", caixas.value);
+})
+
+sucata_pesada.value =  localStorage.getItem("sucata_pesada")
+sucata_mista.value = localStorage.getItem("sucata_mista")
+Aluminio.value = localStorage.getItem("Aluminio")
+Cobre.value = localStorage.getItem("Cobre")
+Metal.value = localStorage.getItem("Metal")
+Ferro_Fundido.value = localStorage.getItem("Ferro_Fundido")
+Ferro_Ruim.value = localStorage.getItem("Ferro_Ruim")
+caixas.value = localStorage.getItem("caixa")
